a8a970bc992faef26d428a452373e512fa7b36b0
mca380 <mcanovas@brujula.es>
2017-12-15 14:46:17 +0100

    canvisdef

--- document.js
+++ document.js
@@ -1,7 +1,21 @@
-function esprimer (numero)
-for (var divisor=2; divisor<numero; divisor++) {
-    if (numero%divisor ==0){
-        return false;
+function Calcular() {/*Si és major o igual a 1 i no és un isNaN posar titol*/ 
+    var nombre = document.getElementById('nombre').value;
+    if(nombre>=1 && !isNaN (nombre)){
+        $('#taulanombresprimers tr:last').after('<tr><th> Nombres primers fins al ' +nombre+ '</th><tr>');
+        for (var i = 1; i <= nombre; i++) {
+            if(esprimer(i)){/*Entra al bucle de la funció esprimer*/
+                $('#taulanombresprimers tr:last').after('<tr><td>' + "<br>" +i+ '</td><td>' + '</td></tr>'); 
+            }
+        }
+    } else {
+        alert ("No has introduit un valor vàlid");
     }
-}
-return true;
+} 
+function esprimer (nombre) {/*Funció principal: treu el nombres primers*/
+    for (var divisor=2; divisor<nombre; divisor++) {
+        if (nombre%divisor ==0){
+            return false;
+        }
+    }
+    return true;
+}          
